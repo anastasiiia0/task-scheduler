3e376616e2678ace1cdce4ff7bac9d591949814f
anastasiyaaa0 <anastasiiiia98@gmail.com>
2024-05-09 16:55:41 +0300

    remove useless comments

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -6,20 +6,6 @@ export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
 export const selectStatusFilter = state => state.filters.status;
 
-// export const selectVisibleTasks1 = state => {
-//   const tasks = selectTasks(state);
-//   const statusFilter = selectStatusFilter(state);
-
-//   switch (statusFilter) {
-//     case statusFilters.active:
-//       return tasks.filter(task => !task.completed);
-//     case statusFilters.completed:
-//       return tasks.filter(task => task.completed);
-//     default:
-//       return tasks;
-//   }
-// };
-
 export const selectVisibleTasks = createSelector(
   [selectTasks, selectStatusFilter],
   (tasks, statusFilter) => {
